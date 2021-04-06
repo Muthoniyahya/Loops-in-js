362238c807011906cdb1645e9ee0f0b813b6fadc
student <Muthoniyahya@gmail.com>
2021-04-06 11:10:41 +0300

    first commit

---
+++ truthy.js
@@ -0,0 +1,20 @@
+var human={firstName:"Iman",lastName:"Yahya",age:18};
+var names=" ";
+var y;
+for(y in human){
+ names+=human[y] +" ";   
+
+
+}
+console.log(names);
+
+
+var cities=['Nairobi','Mombasa','Nyeri'];
+var x;
+for(x of cities){
+    console.log(x)
+
+}
+
+
+
